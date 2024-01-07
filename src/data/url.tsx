a315ee52e627ab5895f05889cf6ba4aa86218331
NgZiXin <ngzixin02@gmail.com>
2024-01-08 00:54:31 +0800

    Update url.tsx

--- src/data/url.tsx
+++ src/data/url.tsx
@@ -1,6 +1,6 @@
 // Api url
 
-const url = "https://sheltered-peak-92103-0a4ca6ccd44f.herokuapp.com/"; // Production Api url
+const url = "https://agile-bayou-58643-6a3a02c4aac8.herokuapp.com"; // Production Api url
 // const url = "http://localhost:3000"; // Development Api url
 
 export default url;
