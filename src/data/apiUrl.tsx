df9912a1631e1e1f4c4cf6f0caf1cb9b2eb9059a
NgZiXin <ngzixin02@gmail.com>
2024-01-08 22:30:40 +0800

    Update apiUrl.tsx

--- src/data/apiUrl.tsx
+++ src/data/apiUrl.tsx
@@ -1,6 +1,6 @@
 // Api url
 
-const baseUrl = "https://zixin.me"; // Production Api url
+const baseUrl = "https://www.zixin.me"; // Production Api url
 // const baseUrl = "http://localhost:3000"; // Development Api url
 
 const apiUrl = baseUrl + "/api/v1";
